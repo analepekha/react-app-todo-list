b9ddf215f7b44fb461057b90789350e95e82ee9a
analepekha <maiboroda0103@gmail.com>
2022-12-26 18:35:38 +0200

    bugfix

--- src/components/TodoFilter/TodoFilter.jsx
+++ src/components/TodoFilter/TodoFilter.jsx
@@ -1,21 +1,30 @@
-import { TextField, Box} from "@mui/material";
+import { TextField, Box } from '@mui/material';
 
-export const TodoFilter = ({value, handleFilterChange}) => {
-    
-    return (
-        <Box component="div" sx={{display: 'flex', flexDirection: 'column',
-            alignItems: 'center', mb:'30px',  mt:'20px'}}>
-        <TextField
-            sx={{width:'400px'}}
-            label="Filter tasks"
-            variant="outlined"
-            color="secondary"
-            size="small"
-            type="text"
-            name="filter"
-            value={value}
-            onChange={handleFilterChange}
-            />
-        </Box>
-    )
-}
+export const TodoFilter = ({ value, handleFilterChange }) => {
+  return (
+    <Box
+      component="div"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        mb: '30px',
+        mt: '20px',
+        pr: '30px',
+        pl: '30px',
+      }}
+    >
+      <TextField
+        fullWidth
+        label="Filter tasks"
+        variant="outlined"
+        color="secondary"
+        size="small"
+        type="text"
+        name="filter"
+        value={value}
+        onChange={handleFilterChange}
+      />
+    </Box>
+  );
+};
