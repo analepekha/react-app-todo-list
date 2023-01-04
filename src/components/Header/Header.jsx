e4f8b3b7eae9c9b1e5df4a118216b9d844bb9624
analepekha <maiboroda0103@gmail.com>
2023-01-04 18:30:47 +0200

    done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,23 @@
-import { Typography, Box } from "@mui/material";
+import { Typography, Box } from '@mui/material';
+import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
 
 export const Header = () => {
   return (
     <Box
       component="div"
       sx={{
-        maxWidth: "100%",
-        textAlign: "center",
-        backgroundColor: "primary.main",
-        boxShadow: "2",
+        maxWidth: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'primary.main',
+        boxShadow: '2',
       }}
     >
+      <PlaylistAddCheckIcon color="secondary" />
       <Typography
-        sx={{ p: "15px", textTransform: "uppercase", letterSpacing: "0.2rem" }}
-        variant="h3"
+        sx={{ p: '15px', textTransform: 'uppercase', letterSpacing: '0.2rem' }}
+        variant="h5"
         component="h1"
         color="secondary"
       >
